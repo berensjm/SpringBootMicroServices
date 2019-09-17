1c1fab26ce58f85d5743982ace4613fb82cc7592
Administrator <Administrator@DESKTOP-099UQRG>
2019-09-17 14:46:42 -0400

    Update .gitignore

--- src/main/java/com/PlantPlaces/PlantPlacesController.java
+++ src/main/java/com/PlantPlaces/PlantPlacesController.java
@@ -3,6 +3,13 @@ package com.PlantPlaces;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * 
+ * @author Administrator
+ *Handle the /start endpoing
+ *@return
+ */
+
 @Controller
 
 
